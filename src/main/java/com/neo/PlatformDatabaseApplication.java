bcbb68e1da9727eb5c06c2946ebb35855f82fb57
xiaolin <xiaolin@maizijf.com>
2017-07-26 15:14:55 +0800

    Initial commit

---
+++ src/main/java/com/neo/PlatformDatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformDatabaseApplication.class, args);
+	}
+}
